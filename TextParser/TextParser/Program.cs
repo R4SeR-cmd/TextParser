644bee6eb197a9877fa4ae48657f91bac2bf7a87
R4SeR-cmd <72449672+R4SeR-cmd@users.noreply.github.com>
2025-03-01 12:26:26 +0200

    Finished task

--- TextParser/TextParser/Program.cs
+++ TextParser/TextParser/Program.cs
@@ -4,6 +4,36 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        Dictionary<char,int> dictionary = new Dictionary<char,int>();
 
+
+        Console.WriteLine("Enter text: ");
+        string text = Console.ReadLine();
+
+        foreach (var t in text)
+        {
+            if (t is >= 'A' and <= 'Z' 
+                or >= 'a' and <= 'z' 
+                or ' ' or '!' or '?' or '.' or ',')
+            {
+                if (dictionary.ContainsKey(t))
+                {
+                    dictionary[t]++;
+                }
+
+                else
+                {
+                    dictionary.Add(t, 1);
+                }
+            }
+        }
+
+        List<char> sortedSymbols = dictionary.Keys.ToList();
+        sortedSymbols.Sort();
+
+        foreach (var c in sortedSymbols)
+            Console.WriteLine($"|{c}| : {dictionary[c]}");
     }
+
+    
 }
