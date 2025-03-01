2a636420466572aa5f95cd94af92ad07c04bc8aa
R4SeR-cmd <72449672+R4SeR-cmd@users.noreply.github.com>
2025-03-01 12:52:28 +0200

    Resolve comments

--- TextParser/TextParser/Program.cs
+++ TextParser/TextParser/Program.cs
@@ -1,39 +1,19 @@
-﻿namespace TextParser;
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TextParser;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Dictionary<char,int> dictionary = new Dictionary<char,int>();
-
-
         Console.WriteLine("Enter text: ");
         string text = Console.ReadLine();
 
-        foreach (var t in text)
-        {
-            if (t is >= 'A' and <= 'Z' 
-                or >= 'a' and <= 'z' 
-                or ' ' or '!' or '?' or '.' or ',')
-            {
-                if (dictionary.ContainsKey(t))
-                {
-                    dictionary[t]++;
-                }
+        var characterCounter = new CharacterCounter(text);
+        var sortedChars = characterCounter.CountCharacterFrequencies();
 
-                else
-                {
-                    dictionary.Add(t, 1);
-                }
-            }
-        }
-
-        List<char> sortedSymbols = dictionary.Keys.ToList();
-        sortedSymbols.Sort();
-
-        foreach (var c in sortedSymbols)
-            Console.WriteLine($"|{c}| : {dictionary[c]}");
+        foreach (var kv in sortedChars)
+            Console.WriteLine($"|{kv.Key}| : {kv.Value}");
     }
-
-    
 }
